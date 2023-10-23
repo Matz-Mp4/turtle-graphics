c9f590d85ba06dee54d19b739ba6ff24de162f55
matz <mateus.assalti@gmail.com>
2023-10-23 12:27:32 -0300

    Better draw line functin

--- src/canvas/canvas.rs
+++ src/canvas/canvas.rs
@@ -31,6 +31,69 @@ pub trait Canvas {
     ) where
         Self: Sized,
     {
+        let mut y0 = q1.y().to_i32().unwrap();
+        let y1 = q2.y().to_i32().unwrap();
+        let mut x0 = q1.x().to_i32().unwrap();
+        let x1 = q2.x().to_i32().unwrap();
+
+        let mut dx = x1 - x0;
+        let mut dy = y1 - y0;
+        let mut stepx = 0;
+        let mut stepy = 0;
+
+        if dy < 0 {
+            dy = -dy;
+            stepy = -1;
+        } else {
+            stepy = 1;
+        }
+
+        if dx < 0 {
+            dx = -dx;
+            stepx = 1;
+        } else {
+            stepx = -1;
+        }
+        dy <<= 1;
+        dx <<= 1;
+        self.set_color(x0 as usize, y0 as usize, color);
+        let height = self.height();
+        let width= self.width();
+
+        if dx > dy {
+            let mut fraction = dy - (dx >> 1);
+            while x0 != x1 {
+                if fraction >= 0 {
+                    y0 += stepy;
+                    fraction -= dx;
+                }
+                x0 += stepx;
+                fraction += dy;
+                /* pixel[x0+y0] = pix; */
+                self.set_color(width - (x0 as usize), height - (y0 as usize), color)
+            }
+        } else {
+            let mut fraction = dx - (dy >> 1);
+            while y0 != y1 {
+                if fraction >= 0 {
+                    x0 += stepx;
+                    fraction -= dy;
+                }
+                y0 += stepy;
+                fraction += dx;
+                self.set_color(width - (x0 as usize), height - (y0 as usize), color)
+            }
+        }
+    }
+
+    fn draw_line2<T: Real + Zero>(
+        &mut self,
+        q1: &impl ITuple2<T>,
+        q2: &impl ITuple2<T>,
+        color: Color,
+    ) where
+        Self: Sized,
+    {
         let dx = *q2.x() - *q1.x();
         let dy = *q2.y() - *q1.y();
 
@@ -47,13 +110,13 @@ pub trait Canvas {
 
         let mut x = *q1.x();
         let mut y = *q1.y();
-        let height= self.height();
+        let height = self.height();
 
         let steps = temp.round().to_usize().unwrap();
         for i in 0..=steps {
             let temp_x = x.round().to_usize().unwrap();
             let temp_y = y.round().to_usize().unwrap();
-            self.set_color( temp_x, height - temp_y, color);
+            self.set_color(temp_x, height - temp_y, color);
             x = x + x_inc;
             y = y + y_inc;
         }
