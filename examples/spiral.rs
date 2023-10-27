3a91ec9dd3020f485be0ed077aeee93759001ff2
arthurbartol <arthur.b4rtol@gmail.com>
2023-10-27 10:28:56 -0300

    Added spiral

---
+++ examples/spiral.rs
@@ -0,0 +1,26 @@
+use std::f64::consts::PI;
+
+use turtle_graphics::Canvas;
+use turtle_graphics::Color;
+use turtle_graphics::Tuple2;
+use turtle_graphics::TurtleLogo;
+use turtle_graphics::WindowCanvas;
+
+pub fn spiral(tur: &mut TurtleLogo<f64>, sides: usize, ang: f64, scale: f64, color: Color, can: &mut impl Canvas) {
+    for _ in 0..sides {
+        *tur = tur.forward(1.0, color, can);
+        *tur = tur.turn(ang);
+        *tur = tur.resize(scale);
+    }
+}
+
+fn main() {
+    let mut canvas = WindowCanvas::new(500, 500);
+    let mut turtle = TurtleLogo::new(Tuple2::new(50.0, 200.0), Tuple2::new(400.0, 0.0));
+    canvas.set_background(Color::white());
+    spiral(&mut turtle, 100, (4.0 * PI) / 5.0, 9.0 / 10.0, Color::black(), &mut canvas);
+
+    while !canvas.should_close() {
+        canvas.display();
+    }
+}
