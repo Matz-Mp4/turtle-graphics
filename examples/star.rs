5783294424db814b3bba6e639c90f246abff9657
arthurbartol <arthur.b4rtol@gmail.com>
2023-10-27 10:19:12 -0300

    Added star example

---
+++ examples/star.rs
@@ -0,0 +1,25 @@
+use std::f64::consts::PI;
+
+use turtle_graphics::Canvas;
+use turtle_graphics::Color;
+use turtle_graphics::Tuple2;
+use turtle_graphics::TurtleLogo;
+use turtle_graphics::WindowCanvas;
+
+pub fn star(tur: &mut TurtleLogo<f64>, sides: usize, color: Color, can: &mut impl Canvas) {
+    for _ in 0..sides {
+        *tur = tur.forward(1.0, color, can);
+        *tur = tur.turn(4.0 * PI / sides as f64);
+    }
+}
+
+fn main() {
+    let mut canvas = WindowCanvas::new(500, 500);
+    let mut turtle = TurtleLogo::new(Tuple2::new(100.0, 200.0), Tuple2::new(200.0, 0.0));
+    canvas.set_background(Color::white());
+    star(&mut turtle, 5, Color::black(), &mut canvas);
+
+    while !canvas.should_close() {
+        canvas.display();
+    }
+}
