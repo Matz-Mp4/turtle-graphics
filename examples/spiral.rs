c1832e184b30a80cb44d8eeb98ce55fa8fd9a689
arthurbartol <arthur.b4rtol@gmail.com>
2023-10-27 10:39:41 -0300

    Update

--- examples/spiral.rs
+++ examples/spiral.rs
@@ -6,6 +6,9 @@ use turtle_graphics::Tuple2;
 use turtle_graphics::TurtleLogo;
 use turtle_graphics::WindowCanvas;
 
+use turtle_graphics::BLACK;
+use turtle_graphics::WHITE;
+
 pub fn spiral(tur: &mut TurtleLogo<f64>, sides: usize, ang: f64, scale: f64, color: Color, can: &mut impl Canvas) {
     for _ in 0..sides {
         *tur = tur.forward(1.0, color, can);
@@ -17,8 +20,8 @@ pub fn spiral(tur: &mut TurtleLogo<f64>, sides: usize, ang: f64, scale: f64, col
 fn main() {
     let mut canvas = WindowCanvas::new(500, 500);
     let mut turtle = TurtleLogo::new(Tuple2::new(50.0, 200.0), Tuple2::new(400.0, 0.0));
-    canvas.set_background(Color::white());
-    spiral(&mut turtle, 100, (4.0 * PI) / 5.0, 9.0 / 10.0, Color::black(), &mut canvas);
+    canvas.set_background(WHITE);
+    spiral(&mut turtle, 100, (4.0 * PI) / 5.0, 9.0 / 10.0, BLACK, &mut canvas);
 
     while !canvas.should_close() {
         canvas.display();
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
 pub mod color;
 pub use color::Color;
+pub use color::BLACK;
+pub use color::WHITE;
+pub use color::RED;
+pub use color::GREEN;
+pub use color::BLUE;
+
 pub mod canvas; 
 pub use canvas::Canvas;
 pub use canvas::WindowCanvas;
 pub use canvas::ImageCanvas;
+
 pub mod tuple;
 pub use tuple::Tuple2;
 pub use tuple::ITuple2;
